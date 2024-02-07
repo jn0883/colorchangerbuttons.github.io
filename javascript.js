1c9fbb3ff25a89a83adfade72309b43f07a7a0e9
Jeevesh Patel <103555226+jn0883@users.noreply.github.com>
2024-02-08 04:58:36 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+
+buttons.forEach( function(button){
+    button.addEventListener('click',function(e){
+        if(e.target.id === "black"){
+            body.style.backgroundColor =  e.target.id;
+            
+        }
+        if(e.target.id === "yellow"){
+            body.style.backgroundColor =  e.target.id;
+        }
+        if(e.target.id === "aqua"){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
